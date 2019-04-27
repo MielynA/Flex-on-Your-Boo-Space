6256ab3b182c68bbca065a6357eac60df39919c9
Pamela Abreu <pamelaabreu@pursuit.com>
2019-04-27 17:23:32 -0400

    Created comments for where to put components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,19 @@
 import React from 'react'; 
-
+import {Route} from 'react-router-dom';
 // Pages 
-import Navbar from './components/Navbar'
-import Welcome from './components/Welcome'
-
-
-
+import Navbar from './components/Navbar';
+import Welcome from './components/Welcome';
+// import Home component here;
+// import SpaceCard here;
 
 function App() {
   return (
-    <div className="App">
-    <Navbar/>
-    <Welcome/>
+    <div>
+      <Route to='/' component={Navbar} />
+        <div>
+          {/* <Route to='/' exact component={Home} /> */}
+          {/* <Route to='/spaceCard/:id' exact component={SpaceCard} /> */}
+        </div>
     </div>
   );
 }
