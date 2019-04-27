294a09d8339acec9485199adfc3936e697ffe606
Stephanie Durandisse <stephaniedurandisse@pursuit.org>
2019-04-27 16:43:15 -0400

    Welcome (#7)
    * added navbar
    
    * fixed navbar links
    
    * added welcome

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from 'react'; 
+
+// Pages 
 import Navbar from './components/Navbar'
+import Welcome from './components/Welcome'
+
+
 
 
 function App() {
   return (
     <div className="App">
     <Navbar/>
+    <Welcome/>
     </div>
   );
 }
---
+++ frontend/src/components/Welcome.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// inline styles
+const container = {
+    textAlign: 'center',
+    padding: '100px'
+}
+
+
+const Welcome = () => {
+    return (
+    <div style={ container }>
+        <h1>Welcome to Space Boo</h1>
+    </div>
+    )
+}
+
+export default Welcome;
