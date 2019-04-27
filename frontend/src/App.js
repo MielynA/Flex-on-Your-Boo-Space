0de5ff4eb7ca98606baec1c23324dc86118d7928
sdurandisse3 <stephaniedurandisse@pursuit.org>
2019-04-27 16:31:16 -0400

    added welcome

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from 'react'; 
+
+// Pages 
 import Navbar from './components/Navbar'
+import Welcome from './components/Welcome'
 
 
 function App() {
   return (
     <div className="App">
     <Navbar/>
+    <Welcome/>
     </div>
   );
 }
