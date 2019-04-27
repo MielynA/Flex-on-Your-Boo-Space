701e8e7309b0603b505d7fa61852959c2cc4f22b
Mielyn De Lara-Acosta <43800466+MielynA@users.noreply.github.com>
2019-04-27 18:49:22 -0400

    Search card (#15)
    * created file for searchCard
    
    * added card to show the boospace data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,19 @@
 import React from 'react'; 
 import {Route} from 'react-router-dom';
 // Pages 
-import Navbar from './components/navBar';
+import Navbar from './components/Navbar';
 
 import Home from './components/home';
-// import SpaceCard here;
+import SpaceCard from './containers/searchCard';
 
 function App() {
   return (
     <div>
       <Route to='/' component={Navbar} />
         <div>
-           <Route to='/' exact component={Home} /> 
-          {/* <Route to='/spaceCard/:id' exact component={SpaceCard} /> */}
+          <Route to='/' exact component={Home} />
+          <Route to='/spaceCard/:id' exact component={SpaceCard} />
+
         </div>
     </div>
   );
