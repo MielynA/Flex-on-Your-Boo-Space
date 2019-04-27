4f2313b8cc7c8b6e2a21d23532f99dcd491919e3
Mielyn Acosta <mielynacosta@pursuit.org>
2019-04-27 18:42:07 -0400

    added card to show the boospace data

--- frontend/src/containers/searchCard.js
+++ frontend/src/containers/searchCard.js
@@ -11,7 +11,21 @@ export default class SearchCard extends React.Component {
    render(){
        return(
            <React.Fragment>
-               <p>this is the test</p>
+               <div className="container text-white bg-dark mt-4 mb-4">
+               
+               <h5 className="card-title mt-4">Card title</h5>
+               <img className="card-img-top" src="https://apod.nasa.gov/apod/image/1904/pia23122c-16.jpg" alt="booSpace image"/>
+               <div className="card-body">
+               <div>Date:       ||    Horoscope:     </div>
+               <div>Facts:</div>
+               <p className="card-text">Personal Note:</p>
+               <p>From: (optional) </p>
+               {/* we could do a moment here */}
+               <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+             </div>
+              </div>
+
+               
            </React.Fragment>
        );
    }
