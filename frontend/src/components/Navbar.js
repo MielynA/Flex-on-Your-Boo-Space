e6767696f5b3521c7eb7024ee309e27e261ed826
sdurandisse3 <stephaniedurandisse@pursuit.org>
2019-04-27 18:16:32 -0400

    fixed navbar name

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const NavBar = () => {
+const Navbar = () => {
     return (
 <nav className="navbar navbar-light bg-light">
   <Link className="navbar-brand" to="/">Flex On Your Boo Space</Link>
@@ -17,4 +17,4 @@ const NavBar = () => {
     )
 }
 
-export default NavBar;
+export default Navbar;
