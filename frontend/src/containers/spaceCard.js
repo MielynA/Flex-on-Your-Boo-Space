c18044f9df1fa3bed2d25d7ce7971133fc4319f7
Mielyn Acosta <mielynacosta@pursuit.org>
2019-04-28 10:56:22 -0400

    completed the spacecard functionality

--- frontend/src/containers/spaceCard.js
+++ frontend/src/containers/spaceCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import CardAxiosService from '../services/axios/card';
 import {Link} from 'react-router-dom'
+import moment from 'moment';
 
 export default class SpaceCard extends React.Component { 
     constructor(props){
@@ -12,11 +13,11 @@ export default class SpaceCard extends React.Component {
             fromname: "", 
             horoscropesign: "", 
             id: "", 
-            img_url: null, 
+            img_url: "", 
             personalnote: "", 
             title: "", 
             toname: "",
-            booSpace: [],
+            // booSpace: [],
 
         }
     }
@@ -25,8 +26,31 @@ export default class SpaceCard extends React.Component {
         const id = this.props.match.params.id;
         CardAxiosService.getSpaceCard(id)
         .then(({msg}) => {
-            this.setState({ booSpace: msg })
-            console.log(msg)
+            const {
+                date, 
+                spaceDate, 
+                description, 
+                fromname, 
+                horoscropesign, 
+                id, 
+                img_url, 
+                personalnote, 
+                title, 
+                toname,
+             } = msg;
+             
+
+            this.setState({ date, 
+                spaceDate, 
+                description, 
+                fromname, 
+                horoscropesign, 
+                id, 
+                img_url, 
+                personalnote, 
+                title, 
+                toname })
+            console.log(msg, "msg")
         })
     }
 
@@ -46,24 +70,23 @@ export default class SpaceCard extends React.Component {
            booSpace
         } = this.state;
         console.log('this state', this.state)
-       const withoutName = <p>Dear My Boo;</p>
-       
+       const withoutName = <p>Dear My Boo</p>
+       const fromName = <p></p>
        return(
 
            <React.Fragment>
                <div className="container text-white bg-dark mt-4 mb-4">
-               <h5 className="card-title mb-4 mt-4">{this.state.toname ?  'Dear:' + this.state.toname : withoutName}</h5>
+               <h5 className="card-title mb-4 mt-4">{this.state.toname.length >0  ?  'Dear:' + this.state.toname : withoutName}</h5>
                <img className="card-img-top" src={img_url} alt={title}/>
                <div className="card-body">
-               <div style={{display: 'flex'}}>Date: {date}    </div>  
-               <div style={{}}>Horoscope:  </div>
+               <div style={{display: 'flex'}}>{moment(date).format('MMMM Do YYYY, h:mm:ss a')}   </div>  
+               <div style={{}}>Horoscope:{horoscropesign}  </div>
                <div>Facts: {description}</div>
                <p className="card-text">Personal Note:{personalnote}</p>
-               <p>From: {fromname}</p>
+               {fromname ? <p>From: {fromname}</p> : null}
                {/* we could do a moment here */}
                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-               <button className="btn btn-primary">Like</button>
-               <button className="ml-2 btn btn-primary">Refresh</button>
+               <button className="ml-2 btn btn-primary">Share</button>
              </div>
              </div>
 
