abc6a5798987c4a79196c510aec4e181e12ce686
Srirupa <srirupadatta@pursuit.org>
2019-04-27 21:59:09 -0400

    added state properties; added event listener to input tags; created initial handleSubmit function

--- frontend/src/containers/messageInfo.js
+++ frontend/src/containers/messageInfo.js
@@ -7,41 +7,51 @@ export default class MessageInfo extends React.Component {
         toName: '',
         fromName: '',
         personalNote: '',
+        title: '',
         img_url: '',
-        date: '',
+        spaceDate: '',
         description: ''
     }
 
     handleChange = (e) => {
+        console.log('e', e.target.value)
         this.setState({ [e.target.name]: e.target.value });
     }
 
-
+    handleSubmit = (e) => {
+        e.preventDefault();
+    }
 
     render() {
         const { toName, fromName, personalNote, img_url, date, description } = this.state;
 
         return (
-            <form>
+            <form className='needs-validation'>
                 <div className="col-auto">
-                    <label className="sr-only" for="inlineFormInputGroup">To</label>
+                    <label className="sr-only" htmlFor="inlineFormInputGroup">To</label>
                     <div className="input-group mb-2">
                         <div className="input-group-prepend">
                             <div className="input-group-text">To:</div>
                         </div>
-                        <input type="text" className="form-control" name='toName' id="inlineFormInputGroup" placeholder="Ya boo's name..." />
+                        <input type="text" className="form-control" onChange={this.handleChange} name='toName' id="inlineFormInputGroup" placeholder="Ya boo's name..." />
                     </div>
                 </div>
                 <div className="col-auto">
-                    <label className="sr-only" for="inlineFormInputGroup">From:</label>
+                    <label className="sr-only" htmlFor="inlineFormInputGroup">From:</label>
                     <div className="input-group mb-2">
                         <div className="input-group-prepend">
                             <div className="input-group-text">From:</div>
                         </div>
-                        <input type="text" className="form-control" name='fromName' id="inlineFormInputGroup" placeholder="(Optional) Your name..." />
+                        <input type="text" className="form-control" onChange={this.handleChange} name='fromName' id="inlineFormInputGroup" placeholder="(Optional) Your name..." />
                     </div>
                 </div>
-                <input type="date" name="bday" />
+                <div className=''>
+                    <label>Choose your space fate...</label>
+                    <input type="date" onChange={this.handleChange} name="spaceDate" />
+                    <div className="invalid-tooltip">
+                        Please provide a date!
+                    </div>
+                </div>
                 <textarea className="form-control" name='personalNote' id="exampleFormControlTextarea1" rows="2"></textarea>
                 <button type="submit" className="btn btn-primary my-2">Submit</button>
             </form>
