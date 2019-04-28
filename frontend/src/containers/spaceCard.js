880669c96f4725faa5c5fed91879a1071235d1fe
Pamela Abreu <pamelaabreu@pursuit.com>
2019-04-27 20:40:27 -0400

    Created componentDidMount functionality

--- frontend/src/containers/spaceCard.js
+++ frontend/src/containers/spaceCard.js
@@ -1,20 +1,52 @@
 import React from 'react';
+import CardAxiosService from '../services/axios/card';
 
 export default class SpaceCard extends React.Component { 
     constructor(props){
         super(props);
         this.state = {
-
+            date: "", 
+            spaceDate: "", 
+            description: "", 
+            fromname: "", 
+            horoscropesign: "", 
+            id: "", 
+            img_url: "", 
+            personalnote: "", 
+            title: "", 
+            toname: ""
 
         }
     }
+
+    componentDidMount () {
+        const id = this.props.match.params.id;
+        CardAxiosService.getSpaceCard(id)
+        .then(({msg}) => {
+            this.setState({ state: msg })
+        })
+    }
+
    render(){
+       const {
+           date, 
+           spaceDate, 
+           description, 
+           fromname, 
+           horoscropesign, 
+           id, 
+           img_url, 
+           personalnote, 
+           title, 
+           toname
+        } = this.state;
+
        return(
            <React.Fragment>
                <div className="container text-white bg-dark mt-4 mb-4">
                
                <h5 className="card-title mt-4">Card title</h5>
-               <img className="card-img-top" src="https://apod.nasa.gov/apod/image/1904/pia23122c-16.jpg" alt="booSpace image"/>
+               <img className="card-img-top" src={img_url} alt={title}/>
                <div className="card-body">
                <div>Date:       ||    Horoscope:     </div>
                <div>Facts:</div>
