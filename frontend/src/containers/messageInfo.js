e23677005aa27806b96106a554f6c4d94204352a
Srirupa <srirupadatta@pursuit.org>
2019-04-27 20:47:49 -0400

    added state properties; added event handler for user inputs; added name attribute to input tags

--- frontend/src/containers/messageInfo.js
+++ frontend/src/containers/messageInfo.js
@@ -4,36 +4,46 @@ import NasaAxiosService from '../services/axios/nasa';
 export default class MessageInfo extends React.Component {
 
     state = {
-        toInput: '',
-        fromInput: '',
+        toName: '',
+        fromName: '',
+        personalNote: '',
+        img_url: '',
         date: '',
-        note: ''
+        description: ''
     }
 
+    handleChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+
+
     render() {
+        const { toName, fromName, personalNote, img_url, date, description } = this.state;
+
         return (
             <form>
-                <div class="col-auto">
-                    <label class="sr-only" for="inlineFormInputGroup">To</label>
-                    <div class="input-group mb-2">
-                        <div class="input-group-prepend">
-                            <div class="input-group-text">To:</div>
+                <div className="col-auto">
+                    <label className="sr-only" for="inlineFormInputGroup">To</label>
+                    <div className="input-group mb-2">
+                        <div className="input-group-prepend">
+                            <div className="input-group-text">To:</div>
                         </div>
-                        <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="Ya boo's name..." />
+                        <input type="text" className="form-control" name='toName' id="inlineFormInputGroup" placeholder="Ya boo's name..." />
                     </div>
                 </div>
-                <div class="col-auto">
-                    <label class="sr-only" for="inlineFormInputGroup">From:</label>
-                    <div class="input-group mb-2">
-                        <div class="input-group-prepend">
-                            <div class="input-group-text">From:</div>
+                <div className="col-auto">
+                    <label className="sr-only" for="inlineFormInputGroup">From:</label>
+                    <div className="input-group mb-2">
+                        <div className="input-group-prepend">
+                            <div className="input-group-text">From:</div>
                         </div>
-                        <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="(Optional) Your name..." />
+                        <input type="text" className="form-control" name='fromName' id="inlineFormInputGroup" placeholder="(Optional) Your name..." />
                     </div>
                 </div>
                 <input type="date" name="bday" />
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="2"></textarea>
-                <button type="submit" class="btn btn-primary my-2">Submit</button>
+                <textarea className="form-control" name='personalNote' id="exampleFormControlTextarea1" rows="2"></textarea>
+                <button type="submit" className="btn btn-primary my-2">Submit</button>
             </form>
         )
     }
